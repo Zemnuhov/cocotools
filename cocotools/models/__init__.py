1c070def91deb0674141313d3e2e5c8ab1d6c644
Zemnuhov <zemnuhov2405@gmail.com>
2026-01-23 15:33:14 +0300

    Update imports

---
+++ cocotools/models/__init__.py
@@ -0,0 +1,15 @@
+from cocotools.models.annotation import CocoAnnotation
+from cocotools.models.category import CocoCategory
+from cocotools.models.dataset import CocoDataset
+from cocotools.models.image import CocoImage
+from cocotools.models.info import CocoInfo
+from cocotools.models.license import CocoLicense
+
+__all__ = [
+    "CocoAnnotation",
+    "CocoCategory",
+    "CocoImage",
+    "CocoInfo",
+    "CocoLicense",
+    "CocoDataset"
+]
